1bd3609904995182b1763c1401053efee217f280
Lanie Williams <lanie_williams@baylor.edu>
2022-03-21 14:16:37 -0500

    pandas assignment

---
+++ pandas_assignment.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+print("1.Produce that had the highest and lowest sales in total sales (both name of produce and value)")
+
+
+
+
+
+print("2. Using 'loc', display the quantity and total sales for 'Orange' and 'Beets' (together)")
+
+
+
+
+
+print("3.Using 'loc', display the total sales for 'Apples' through 'Lettuce'")
+
+
+
+
+print("4.Using 'at', update the quantity sold for Apricots to 11,955 and total sales to 44,353.05")
+
+
+
+
+print("5.What is the average quantity sold across all products? (print out ONLY quantity sold)")
+
+
+
+
+
+print("6.Create a new dataframe for only those produce that have sold between 11,500 to 12,000 (quantity)")
+
+
+
+
+
+
+print("7.What is the total sales for the products in the above new dataframe? (print out ONLY total sales)")
